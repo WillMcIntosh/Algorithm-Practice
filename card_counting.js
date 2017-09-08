b2fb06daea145994f470a8499df9d06d05fe3274
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-09-07 21:12:26 -0400

    commented my code a bit so I can study later.

--- card_counting.js
+++ card_counting.js
@@ -1,10 +1,11 @@
 var count = 0;
 
 function cc(card) {
-  
+// increment the count if card value is <= 6  
   if (card <= 6){
     count++;
   }
+// decrement the count for high cards
   switch (card){
     case 10:
     case 'J':
@@ -13,7 +14,8 @@ function cc(card) {
     case 'A':
       count--;
   }
-
+// every time the function is called it will change the count
+// and return the count and what to do. Vegas here I don't come.
   if (count <= 0) {
     return count + " Hold";
   } else {
--- find_a_palindrome.js
+++ find_a_palindrome.js
@@ -1,10 +1,14 @@
 function checkPalindrome(inputString) {
+//take the input string and put it into an array, splitting with ''
   let arr = inputString.split('');
   let revArr = [];
   let newString = '';
+// so from length -1 to 0 indices in the array, take each one and pus h it to a new array
     for( let i=arr.length-1;i>=0;i--){
     revArr.push(arr[i]);
   }
   newString = revArr.join('');
+// instead of having to write if (x === y) return true, can
+// just return the output of the test. neater. 
   return inputString === newString
 }
